a583422bcb9ec205e7cbd881f7ad0663e58e6fbd
xela6134 <leol3o05@gmail.com>
2025-04-03 23:42:27 +1100

    Simple UDP client created

---
+++ client.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+#include <csignal>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define SERVER_PORT 8000
+
+int sockfd;
+
+void handleSigint(int sig) {
+    std::cout << "\nGracefully closing server...\n";
+    close(sockfd);
+    exit(0);
+}
+
+int main() { 
+    signal(SIGINT, handleSigint);
+
+    // IPv4 socket address structure
+    // Holds server's IP address (sin_addr) and port number (sin_port)
+    struct sockaddr_in serverAddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;                        // Always set to AF_INET (IPv4)
+    serverAddr.sin_port = htons(SERVER_PORT);               // Numbers are stored in memory in network byte order (most significant byte first)
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);  // Converts IP address into binary form
+
+    if (sockfd < 0) {
+        perror("UDP socket creation failed");
+        return 1;
+    }
+
+    std::string message;
+    std::cout << "------------------------------------------\n";
+    while (true) {
+        std::cout << "Enter message: ";
+        std::getline(std::cin, message);
+
+        ssize_t sent = sendto(sockfd, message.c_str(), message.size(), 0,
+                              (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+        if (sent < 0) {
+            perror("Failed to send!");
+            std::cout << "------------------------------------------\n";
+            continue;
+        }
+
+        std::cout << "You have sent: " << message << std::endl;
+        std::cout << "------------------------------------------\n";
+    }
+
+    close(sockfd);
+    return 0;
+}
